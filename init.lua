cac072aec764ee5cbca411632a975a29edfe4db1
brv-t <126668721+brv-t@users.noreply.github.com>
2026-06-11 10:56:39 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,28 @@
+-- Enable true colors
 vim.o.termguicolors = true
-vim.cmd.colorscheme("solarized")
+
+-- Setup lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("solarized")
+    end,
+  },
+})
